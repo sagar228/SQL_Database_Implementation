29b3f928c69c49f6b92013f63310a7903a1edcb5
sagar228 <49792067+sagar228@users.noreply.github.com>
2019-09-24 22:28:42 -0700

    FrontEnd Code Block

---
+++ Be_Pro.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Menu1_MenuItemClick(object sender, MenuEventArgs e)
+    {
+        int index = Int32.Parse(e.Item.Value);
+        MultiView1.ActiveViewIndex = index;
+    }
+
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("AdditionalQueries.aspx");
+    }
+}
---
+++ LoginPage.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Configuration;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+//using System.Data.SqlClient;
+using Oracle.DataAccess.Client;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Web.Security;
+
+public partial class LoginPage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblErrorMessage.Visible = false;
+    }
+
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+            OracleConnection orclcon = new OracleConnection("Data Source=orcl;Persist Security Info=True;User ID=DBCactus;Password=DBC123Temp");
+            OracleCommand com;
+            OracleDataAdapter orada;
+            string str;
+            DataTable dt;
+            int RowCount;
+
+            string UserName = txtUserName.Text.Trim();
+            string Password = txtPassword.Text.Trim();
+            orclcon.Open();
+            str = "Select * from login_info";
+            com = new OracleCommand(str);
+            orada = new OracleDataAdapter(com.CommandText, orclcon);
+            dt = new DataTable();
+            orada.Fill(dt);
+            RowCount = dt.Rows.Count;
+            for (int i = 0; i < RowCount; i++)
+            {
+                UserName = dt.Rows[i]["UserName"].ToString();
+                Password = dt.Rows[i]["Password"].ToString();
+                if (UserName == txtUserName.Text && Password == txtPassword.Text)
+                {
+                    Session["UserName"] = UserName;
+                    Response.Redirect("Be_Pro.aspx");
+                }
+                else
+                {
+                    lblErrorMessage.Visible=true;
+                }
+            }
+        }
+    }
